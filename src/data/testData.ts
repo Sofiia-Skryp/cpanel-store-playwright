a704fc63084bd3bb1df589eadbe2ceb5f318b9b3
tjhelpers <sofisk04@gmail.com>
2025-06-03 17:03:31 +0300

    Remove node_modules, playwright-report and test-results from tracking

---
+++ src/data/testData.ts
@@ -0,0 +1,2 @@
+export const PRODUCT_CPANEL_PREMIER = 'cPanel Premier (100 Accounts)';
+export const PRODUCT_CLOUD_LINUX = 'Monthly CloudLinux for cPanel License';
--- src/fixtures/testData.ts
+++
@@ -1,1 +0,0 @@
-export const TEST_IP = '2.2.2.2';
